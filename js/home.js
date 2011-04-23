da66eb470e56f9fbb60a449bc31dc1f635247bef
Pete Brousalis <brousapg@gmail.com>
2011-04-23 15:37:34 -0400

    Raphaeljs madness

--- js/home.js
+++ js/home.js
@@ -1,13 +1,88 @@
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(g(c){g 1N(E){8 x=E.12(\'.20\',\'\');N x.12(\'#\',\'\')}g 1z(l){6(l.t==\'z\'){l.t=0}o 6(l.t==\'1C\'||l.t==\'1B\'||l.t==\'1A\'){l.t=0.5}o 6(l.t==\'1Z\'){l.t=1}6(l.u==\'A\'){l.u=0}o 6(l.u==\'1C\'||l.u==\'1B\'||l.u==\'1A\'){l.u=0.5}o 6(l.u==\'26\'){l.u=1}}g 1L(4,7,v){8 r={x:0,y:0,13:0};6(!4.M){r.x=v.x-7.x;r.y=v.y-7.y;r.13=24.1X(r.x*r.x+r.y*r.y);v.x=7.x+r.x*4.T;v.y=7.y+r.y*4.T;6(r.13<4.U&&r.13>4.U*-1){4.M=C}}o{v.x=7.x;v.y=7.y}}g 1o(q,4){8 m=4.9.m();c.w(q,{h:q.9.h(),j:q.9.j()});c.w(4,{h:4.9.h(),j:4.9.j(),A:m.A,z:m.z})}g 1e(f,1b,1D){8 m;8 P;6(1W(f)===\'1V\'){6(f.1E(/^\\d+\\s?R$/)!=-1){f=f.12(\'R\',\'\');f=1H(f,10);m=1b*(1D-f);P=1b*15+\'%\';N{f:f,G:C,m:m,P:P}}o 6(f.1E(/^\\d+\\s?%$/)!=-1){f.12(\'%\',\'\');f=1H(f,10)/15}o{f=1}}m=1b*(1-f);N{f:f,G:k,m:m}}g 1n(3,i,4){8 F;8 H;8 p={};3[i]=c.w({},{h:3[i].9.h(),j:3[i].9.j()},3[i]);F=1e(3[i].Q,3[i].t,3[i].h);H=1e(3[i].S,3[i].u,3[i].j);c.w(3[i],{1l:4.h/(3[i].h-4.h),1p:4.j/(3[i].j-4.j),Q:F.f,S:H.f,1s:F.G,1u:H.G,1r:F.m,1K:H.m});6(F.G){p.z=F.P}6(H.G){p.A=H.P}6(F.G||H.G){3[i].9.1i(p)}}g 1Q(3,i,1j){8 1a;c.w(3[i],{b:[]});J(8 n=0;n<3[i].9.I().K;n++){6(!3[i].b[n])3[i].b[n]={};6(!3[i].b[n].9)3[i].b[n][\'9\']=3[i].9.I().1S(n);6(!3[i].b[n].18&&3[i].b[n].9.I(\'a\').1y(\'1J\')){3[i].b[n][\'18\']=3[i].b[n].9.I(\'a\').1y(\'1J\')}6(3[i].b[n].18){1a=3[i].b[n].9.m();c.w(3[i].b[n],{h:3[i].b[n].9.h(),j:3[i].b[n].9.j(),x:1a.z-1j.z,y:1a.A-1j.A});c.w(3[i].b[n],{1U:(3[i].b[n].x+3[i].b[n].h/2)/3[i].h,1M:(3[i].b[n].y+3[i].b[n].j/2)/3[i].j})}}}g 1k(3,1q,1x){8 W;8 X;8 p;J(8 i=0;i<3.K;i++){W=3[i].Q*1q+3[i].1r;X=3[i].S*1x+3[i].1K;p={};6(3[i].11){6(3[i].1s){p.1v=W*-1+\'R\'}o{p.z=W*15+\'%\';p.1v=W*3[i].h*-1+\'R\'}}6(3[i].16){6(3[i].1u){p.1t=X*-1+\'R\'}o{p.A=X*15+\'%\';p.1t=X*3[i].j*-1+\'R\'}}3[i].9.1i(p)}}c.1c.V=g(1F){8 e=c().w({},c.1c.V.e,1F);8 1h={11:e.11,16:e.16,t:e.t,u:e.u,Q:e.Q,S:e.S};8 1g={9:e.4,T:e.T,U:e.U};6(e.4)1g[\'9\']=e.4;8 1d=[];J(8 a=1;a<1T.K;a++){1d.2g(c.w({},1h,1T[a]))}N 1f.2d(g(){8 7={x:0.5,y:0.5};8 v={x:0.5,y:0.5};8 B={O:k,1O:e.1I,19:g(x,y){1L(4,7,v);1k(3,v.x,v.y);1f.O=2c(g(){6(7.x!=x||7.y!=y||!4.M){B.19(7.x,7.y)}o 6(B.O){B.O=k}},B.1O)}};8 q={9:c(1f)};8 4=c.w({},{9:q.9},1g,{Y:k,Z:k,L:k,M:k});8 3=[];g 1R(3,E,1P){J(8 i=0;i<3.K;i++){8 2a=k;J(8 n=0;n<3[i].b.K;n++){6(3[i].b[n].18==E){1P(i,n);N[i,n]}}}N k}1o(q,4);J(8 i=0;i<q.9.I().K;i++){3[i]=c.w({},1h,1d[i],{9:q.9.I(\'*:1S(\'+i+\')\')});1n(3,i,4);6(e.1m){1Q(3,i,q.9.m())}}q.9.I().1i(\'21\',\'1Y\');1k(3,0.5,0.5);6(e.1w){c().2b(g(D){4.Y=(D.17>=4.z&&D.17<4.h+4.z)?C:k;4.Z=(D.14>=4.A&&D.14<4.j+4.A)?C:k;6(4.Y&&4.Z&&!4.L){4.M=k;4.L=C}6(4.L){6(4.Y){7.x=(D.17-4.z)/4.h}o{7.x=(D.17<4.z)?0:1}6(4.Z){7.y=(D.14-4.A)/4.j}o{7.y=(D.14<4.A)?0:1}}6(4.Y&&4.Z){6(!B.O)B.19(7.x,7.y)}o 6(4.L){4.L=k}})}6(e.1m){q.9.22("V",g(27,E){E=1N(E);1R(3,E,g(i,n){7.x=3[i].b[n].1U*(3[i].1l+1)-(0.5*3[i].1l);7.y=3[i].b[n].1M*(3[i].1p+1)-(0.5*3[i].1p);6(!e.1G){6(7.x<0)7.x=0;6(7.x>1)7.x=1;6(7.y<0)7.y=0;6(7.y>1)7.y=1}4.M=k;6(!B.O)B.19(7.x,7.y)})})}c(2e).2f(g(){1o(q,4);J(8 i=0;i<3.K;i++){1n(3,i,4)}})})};c.1c.V.e={1w:C,29:k,1m:C,1G:k,11:C,16:C,t:0.5,u:0.5,Q:1,S:1,T:0.28,U:0.23,1I:25};1z(c.1c.V.e);c(g(){})})(c);',62,141,'|||layer|mouseport||if|localmouse|var|element||content|jQuery||settings|travel|function|width||height|false||offset||else|cssObject|viewport|difference||xorigin|yorigin|virtualmouse|extend|||left|top|timer|true|mouse|ref|xStuff|travelpx|yStuff|children|for|length|active|ontarget|return|running|cssPos|xtravel|px|ytravel|takeoverFactor|takeoverThresh|jparallax|xpos|ypos|xinside|yinside||xparallax|replace|sum|pageY|100|yparallax|pageX|anchor|fire|contentOffset|origin|fn|layersettings|parseTravel|this|settingsMouseport|settingsLayer|css|viewportOffset|moveLayers|diffxrat|triggerResponse|setupLayer|setupPorts|diffyrat|xratio|xoffset|xtravelpx|marginTop|ytravelpx|marginLeft|mouseResponse|yratio|attr|initOrigin|center|centre|middle|dimension|search|options|triggerExposesEdges|parseInt|frameDuration|name|yoffset|positionMouse|posyrat|stripFiletype|frame|callback|setupLayerContents|matrixSearch|eq|arguments|posxrat|string|typeof|sqrt|absolute|right|html|position|bind|002|Math||bottom|event|65|mouseActiveOutside|gotcha|mousemove|setTimeout|each|window|resize|push'.split('|'),0,{}))
 
+// jquery doc
 $(document).ready(function($) {
     $('#parallax').jparallax({}, {ytravel:0.5});
+
     $('#top-menu a').animate({opacity: '0.6'},{duration: 300});
     $('#top-menu a').hover(function() {
       $(this).animate({opacity: '1'},{duration: 0});
     }, function() {
       $(this).animate({opacity: '0.6'},{duration: 300});
     });
+    
+    $('.projects li').hover(function() {
+      $(this).find('span').animate({backgroundColor:'#000'},300);
+    }, function () {
+      $(this).find('span').animate({backgroundColor:'#a4a4a4'},300);
+    });
+
+    var today=new Date(); 
+    var bday=new Date(today.getFullYear(), 11, 16); if (today.getMonth()==11 && today.getDate()>16) bday.setFullYear(bday.getFullYear()+1); var days_left = Math.ceil(((bday.getTime()-today.getTime())/(1000*60*60*24))/365*100)
+    var college=new Date(today.getFullYear(), 4, 28); if (today.getMonth() == 4 && today.getDate() > 28) college = 100; var college_days_left = 100-Math.ceil(((college.getTime()-today.getTime())/(1000*60*60*24))/365*100)
+    $('#wrapper').append('<div class="get"><div class="arc"><span class="text">College</span><input type="hidden" class="percent" value="'+college_days_left+'" /><input type="hidden" class="color" value="#97BE0D" /></div><div class="arc"><span class="text">Age 22</span><input type="hidden" class="percent" value="'+days_left+'" /><input type="hidden" class="color" value="#88B8E6" /></div><div class="arc"><span class="text">Something</span><input type="hidden" class="percent" value="50" /><input type="hidden" class="color" value="#BEDBE9" /></div></div>');
 });
 
-// jparallax
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(g(c){g 1N(E){8 x=E.12(\'.20\',\'\');N x.12(\'#\',\'\')}g 1z(l){6(l.t==\'z\'){l.t=0}o 6(l.t==\'1C\'||l.t==\'1B\'||l.t==\'1A\'){l.t=0.5}o 6(l.t==\'1Z\'){l.t=1}6(l.u==\'A\'){l.u=0}o 6(l.u==\'1C\'||l.u==\'1B\'||l.u==\'1A\'){l.u=0.5}o 6(l.u==\'26\'){l.u=1}}g 1L(4,7,v){8 r={x:0,y:0,13:0};6(!4.M){r.x=v.x-7.x;r.y=v.y-7.y;r.13=24.1X(r.x*r.x+r.y*r.y);v.x=7.x+r.x*4.T;v.y=7.y+r.y*4.T;6(r.13<4.U&&r.13>4.U*-1){4.M=C}}o{v.x=7.x;v.y=7.y}}g 1o(q,4){8 m=4.9.m();c.w(q,{h:q.9.h(),j:q.9.j()});c.w(4,{h:4.9.h(),j:4.9.j(),A:m.A,z:m.z})}g 1e(f,1b,1D){8 m;8 P;6(1W(f)===\'1V\'){6(f.1E(/^\\d+\\s?R$/)!=-1){f=f.12(\'R\',\'\');f=1H(f,10);m=1b*(1D-f);P=1b*15+\'%\';N{f:f,G:C,m:m,P:P}}o 6(f.1E(/^\\d+\\s?%$/)!=-1){f.12(\'%\',\'\');f=1H(f,10)/15}o{f=1}}m=1b*(1-f);N{f:f,G:k,m:m}}g 1n(3,i,4){8 F;8 H;8 p={};3[i]=c.w({},{h:3[i].9.h(),j:3[i].9.j()},3[i]);F=1e(3[i].Q,3[i].t,3[i].h);H=1e(3[i].S,3[i].u,3[i].j);c.w(3[i],{1l:4.h/(3[i].h-4.h),1p:4.j/(3[i].j-4.j),Q:F.f,S:H.f,1s:F.G,1u:H.G,1r:F.m,1K:H.m});6(F.G){p.z=F.P}6(H.G){p.A=H.P}6(F.G||H.G){3[i].9.1i(p)}}g 1Q(3,i,1j){8 1a;c.w(3[i],{b:[]});J(8 n=0;n<3[i].9.I().K;n++){6(!3[i].b[n])3[i].b[n]={};6(!3[i].b[n].9)3[i].b[n][\'9\']=3[i].9.I().1S(n);6(!3[i].b[n].18&&3[i].b[n].9.I(\'a\').1y(\'1J\')){3[i].b[n][\'18\']=3[i].b[n].9.I(\'a\').1y(\'1J\')}6(3[i].b[n].18){1a=3[i].b[n].9.m();c.w(3[i].b[n],{h:3[i].b[n].9.h(),j:3[i].b[n].9.j(),x:1a.z-1j.z,y:1a.A-1j.A});c.w(3[i].b[n],{1U:(3[i].b[n].x+3[i].b[n].h/2)/3[i].h,1M:(3[i].b[n].y+3[i].b[n].j/2)/3[i].j})}}}g 1k(3,1q,1x){8 W;8 X;8 p;J(8 i=0;i<3.K;i++){W=3[i].Q*1q+3[i].1r;X=3[i].S*1x+3[i].1K;p={};6(3[i].11){6(3[i].1s){p.1v=W*-1+\'R\'}o{p.z=W*15+\'%\';p.1v=W*3[i].h*-1+\'R\'}}6(3[i].16){6(3[i].1u){p.1t=X*-1+\'R\'}o{p.A=X*15+\'%\';p.1t=X*3[i].j*-1+\'R\'}}3[i].9.1i(p)}}c.1c.V=g(1F){8 e=c().w({},c.1c.V.e,1F);8 1h={11:e.11,16:e.16,t:e.t,u:e.u,Q:e.Q,S:e.S};8 1g={9:e.4,T:e.T,U:e.U};6(e.4)1g[\'9\']=e.4;8 1d=[];J(8 a=1;a<1T.K;a++){1d.2g(c.w({},1h,1T[a]))}N 1f.2d(g(){8 7={x:0.5,y:0.5};8 v={x:0.5,y:0.5};8 B={O:k,1O:e.1I,19:g(x,y){1L(4,7,v);1k(3,v.x,v.y);1f.O=2c(g(){6(7.x!=x||7.y!=y||!4.M){B.19(7.x,7.y)}o 6(B.O){B.O=k}},B.1O)}};8 q={9:c(1f)};8 4=c.w({},{9:q.9},1g,{Y:k,Z:k,L:k,M:k});8 3=[];g 1R(3,E,1P){J(8 i=0;i<3.K;i++){8 2a=k;J(8 n=0;n<3[i].b.K;n++){6(3[i].b[n].18==E){1P(i,n);N[i,n]}}}N k}1o(q,4);J(8 i=0;i<q.9.I().K;i++){3[i]=c.w({},1h,1d[i],{9:q.9.I(\'*:1S(\'+i+\')\')});1n(3,i,4);6(e.1m){1Q(3,i,q.9.m())}}q.9.I().1i(\'21\',\'1Y\');1k(3,0.5,0.5);6(e.1w){c().2b(g(D){4.Y=(D.17>=4.z&&D.17<4.h+4.z)?C:k;4.Z=(D.14>=4.A&&D.14<4.j+4.A)?C:k;6(4.Y&&4.Z&&!4.L){4.M=k;4.L=C}6(4.L){6(4.Y){7.x=(D.17-4.z)/4.h}o{7.x=(D.17<4.z)?0:1}6(4.Z){7.y=(D.14-4.A)/4.j}o{7.y=(D.14<4.A)?0:1}}6(4.Y&&4.Z){6(!B.O)B.19(7.x,7.y)}o 6(4.L){4.L=k}})}6(e.1m){q.9.22("V",g(27,E){E=1N(E);1R(3,E,g(i,n){7.x=3[i].b[n].1U*(3[i].1l+1)-(0.5*3[i].1l);7.y=3[i].b[n].1M*(3[i].1p+1)-(0.5*3[i].1p);6(!e.1G){6(7.x<0)7.x=0;6(7.x>1)7.x=1;6(7.y<0)7.y=0;6(7.y>1)7.y=1}4.M=k;6(!B.O)B.19(7.x,7.y)})})}c(2e).2f(g(){1o(q,4);J(8 i=0;i<3.K;i++){1n(3,i,4)}})})};c.1c.V.e={1w:C,29:k,1m:C,1G:k,11:C,16:C,t:0.5,u:0.5,Q:1,S:1,T:0.28,U:0.23,1I:25};1z(c.1c.V.e);c(g(){})})(c);',62,141,'|||layer|mouseport||if|localmouse|var|element||content|jQuery||settings|travel|function|width||height|false||offset||else|cssObject|viewport|difference||xorigin|yorigin|virtualmouse|extend|||left|top|timer|true|mouse|ref|xStuff|travelpx|yStuff|children|for|length|active|ontarget|return|running|cssPos|xtravel|px|ytravel|takeoverFactor|takeoverThresh|jparallax|xpos|ypos|xinside|yinside||xparallax|replace|sum|pageY|100|yparallax|pageX|anchor|fire|contentOffset|origin|fn|layersettings|parseTravel|this|settingsMouseport|settingsLayer|css|viewportOffset|moveLayers|diffxrat|triggerResponse|setupLayer|setupPorts|diffyrat|xratio|xoffset|xtravelpx|marginTop|ytravelpx|marginLeft|mouseResponse|yratio|attr|initOrigin|center|centre|middle|dimension|search|options|triggerExposesEdges|parseInt|frameDuration|name|yoffset|positionMouse|posyrat|stripFiletype|frame|callback|setupLayerContents|matrixSearch|eq|arguments|posxrat|string|typeof|sqrt|absolute|right|html|position|bind|002|Math||bottom|event|65|mouseActiveOutside|gotcha|mousemove|setTimeout|each|window|resize|push'.split('|'),0,{}))
+// codrops up in here - http://tympanus.net/Tutorials/AnimatedSkillsDiagram/
+var o = {
+	init: function(){
+		this.diagram();
+	},
+	random: function(l, u){
+		return Math.floor((Math.random()*(u-l+1))+l);
+	},
+	diagram: function(){
+		var r = Raphael('diagram', 300, 400),
+			rad = 40,
+			defaultText = 'Life',
+			speed = 250;
+		
+		var size = 130;
+		var stroke_width = 20;
+		
+		r.circle(size, size, 50).attr({ stroke: 'none', fill: '#193340' });
+		
+		var title = r.text(size, size, defaultText).attr({
+			font: '16px "proxima-nova-1"',
+			fill: '#fff'
+		}).toFront();
+		
+		r.customAttributes.arc = function(value, color, rad){
+			var v = 3.6*value,
+				alpha = v == 360 ? 359.99 : v,
+				random = o.random(91, 240),
+				a = (random-alpha) * Math.PI/180,
+				b = random * Math.PI/180,
+				sx = size + rad * Math.cos(b),
+				sy = size - rad * Math.sin(b),
+				x = size + rad * Math.cos(a),
+				y = size - rad * Math.sin(a),
+				path = [['M', sx, sy], ['A', rad, rad, 0, +(alpha > 180), 1, x, y]];
+			return { path: path, stroke: color }
+		}
+		
+		$('.get').find('.arc').each(function(i){
+			var t = $(this), 
+				color = t.find('.color').val(),
+				value = t.find('.percent').val(),
+				text = t.find('.text').text();
+			
+			rad += 25;	
+			var z = r.path().attr({ arc: [value, color, rad], 'stroke-width': stroke_width });
+			
+			z.mouseover(function(){
+                this.stop().animate({ 'stroke-width': stroke_width+10, opacity: .75 }, speed*4, 'elastic').toFront();
+				title.stop().animate({ opacity: 0 }, speed, '>', function(){
+					this.attr({ text: text + '\n' + value + '%' }).animate({ opacity: 1 }, speed, '<');
+				});
+            }).mouseout(function(){
+				this.stop().animate({ 'stroke-width': stroke_width, opacity: 1 }, speed*4, 'elastic');
+				title.stop().animate({ opacity: 0 }, speed, '>', function(){
+					title.attr({ text: defaultText }).animate({ opacity: 1 }, speed, '<');
+				});	
+            });
+		});
+		
+	}
+}
+$(function(){ o.init(); });
