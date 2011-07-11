719ec36ff515cb338b15f18e32d944942d4274a4
brousalis <brousapg@gmail.com>
2011-07-10 23:36:04 -0500

    Carg updates

--- js/carg.js
+++ js/carg.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function() {
-  alert("test");
+  jQuery(".nav_container").append(nav());
+  function nav() {
+    var content;
+    content += "<ul>";
+    content += link("http://twitter.com/brousalis","Follow me on <strong>Twitter</strong>");
+    content += link("http://gplus.to/brousalis", "Find me on <strong>Google Plus</strong>");
+    content += link("http://github.com/brousalis", "Fork me on <strong>GitHub</strong>");
+    content += "</ul>";
+    return content;
+  }
+  function link(url, sentence) {
+    return "<li><a href=\"" + url + "\"><span></span>" + sentence + "</a></li>";
+  }
 });
