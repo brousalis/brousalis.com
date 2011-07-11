0ae7748e3b23d5c082566b870a235d674da8b87f
Pete Brousalis <brousapg@gmail.com>
2011-07-11 12:41:32 -0500

    Adding email

--- js/carg.js
+++ js/carg.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
-  jQuery(".nav_container").append(nav());
   function nav() {
     var content = "";
     content += "<ul>";
+    content += link("mailto:brousapg+web.at.gmail.com", "Contact me by <strong>E-mail</strong>", "email");
     content += link("http://twitter.com/brousalis","Follow me on <strong>Twitter</strong>", "twitter");
     content += link("http://gplus.to/brousalis", "Find me on <strong>Google Plus</strong>", "plus");
     content += link("http://github.com/brousalis", "Fork me on <strong>GitHub</strong>", "github");
@@ -11,10 +11,11 @@ jQuery(document).ready(function() {
     return content;
   }
   function link(url, sentence, social){
-    return "<li class=\""+ social +"\"><a href=\"" + url + "\"><span></span>" + sentence + "</a></li>";
+    return "<li class=\""+ social +"\"><a href=\"" + url + "\">" + sentence + "</a></li>";
   }
-
-  var source = jQuery(".cardimgcrop ").find("img").attr("src")
+  jQuery(".nav_container").append(nav()); 
+  
+  var source = jQuery(".cardimgcrop ").find("img").attr("src");
   jQuery(".cardimgcrop ").parent().parent().parent().css("background","url("+source+")");
-  jQuery("div").remove(".cardimgcrop ")
+  jQuery("div").remove(".cardimgcrop ");
 });
