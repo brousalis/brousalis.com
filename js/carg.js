6ff712610c21f474fb0cc10e32a795825ea054d9
brousalis <brousapg@gmail.com>
2011-07-13 23:07:38 -0500

    JS Fix

--- js/carg.js
+++ js/carg.js
@@ -28,21 +28,5 @@ $(function() {
     $('.scroll_pane').jScrollPane(  { maintainPosition: false }).data('jsp').reinitialise();
   });
 
-	setInterval(function(){
-
-		// Selecting onl:wy the visible layers:
-		var versions = $('.textVersion:visible');
-		
-		if(versions.length<2){
-			// If only one layer is visible, show the other
-			$('.textVersion').fadeIn(800);
-		}
-		else{
-			// Hide the upper layer
-			versions.eq(0).fadeOut(800);
-		}
-	},1000);
-
-
 });
 
